232e45f73a553cc4bd46dfd09e039b64fef3f397
D2Diego <dmarques1508@gmail.com>
2024-03-20 20:11:18 -0300

    feat: format first page

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import logoImg from '../../assets/imagens/logo.svg'
+import landingImg from '../../assets/imagens/landing.svg'
+import studyIcon from '../../assets/imagens/icons/study.svg'
+import giveClassesIcon from '../../assets/imagens/icons/give-classes.svg'
+import purpleHeartIcon from '../../assets/imagens/icons/purple-heart.svg'
+
+import './styles.css'
+
+function Landing(){
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logoImg} alt="proffy" />
+                    <h2>Sua plataforma de estudos online.</h2>
+                </div>
+
+                <img 
+                src={landingImg} 
+                alt="Plataforma de estudos" 
+                className="hero-image" 
+                />
+                <div className="buttons-container">
+                    <a href="" className="study">
+                        <img src={studyIcon} alt="Estudar"
+                        className="hero_image"/>
+                        Estudar
+                    </a>
+
+                    <a href="" className="give-classes">
+                        <img src={giveClassesIcon} alt="Estudar" />
+                        Estudar2
+                    </a>
+                </div>
+
+                    <span className="total-connections">
+                        total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+                    </span>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Landing;
